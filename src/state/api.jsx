489f6cbc67541e9646c6391cb2cc78bc815559c3
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2026-01-04 01:45:06 +0530

    fix and issue

--- src/state/api.jsx
+++ src/state/api.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://invetory-management-bnd-dinithi.onrender.com/api/auth", 
+    baseUrl: "https://inventory-management-bnd-dinithi.onrender.com/api/auth",
   }),
   endpoints: (builder) => ({
     signup: builder.mutation({
